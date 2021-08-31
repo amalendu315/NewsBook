1d6a0613e81f8727e91926436c88c1f4a4217fc2
Amalendu Pandey <84476847+amalendu315@users.noreply.github.com>
2021-09-01 02:45:15 +0530

    commit 3

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,10 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: "90%", zIndex: "1" }}>{source}</span>
+          <div className="d-flex" style={{ justifyContent: "flex-end", position: "absolute", right: "0" }}>
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+
           <img
             src={
               imageUrl
